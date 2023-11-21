315c83d4293ef9552cb30b60d7ef55f33c0fbd42
madhuLin <qq10455006@gmail.com>
2023-11-21 18:55:57 +0800

    initialize project

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,20 +1,88 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
+import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
+import java.sql.Time;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.LOGIN_CODE_KEY;
+import static com.hmdp.utils.RedisConstants.LOGIN_CODE_TTL;
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
  * <p>
  * 服务实现类
  * </p>
  *
- * @author 虎哥
- * @since 2021-12-22
+ * @author
+ * @since
  */
+@Slf4j
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        //1.校驗手機
+        if(RegexUtils.isPhoneInvalid(phone)) {
+            return Result.fail("手機號碼格式錯誤!");
+        }
+        //2.生成驗證碼，保存
+        String code = RandomUtil.randomNumbers(6);
+        stringRedisTemplate.opsForValue().set(LOGIN_CODE_KEY+phone, code, LOGIN_CODE_TTL, TimeUnit.MINUTES);
+
+        log.debug("發送驗證碼成功, 驗證碼{}", code);
+
+        return Result.ok();
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        //1.檢驗手機號和驗證碼
+        String phone = loginForm.getPhone();
+        if(RegexUtils.isPhoneInvalid(phone)) {
+            return Result.fail("手機號碼格式錯誤!");
+        }
+        Object cacheCode = session.getAttribute("code");
+        String code = loginForm.getCode();
+        if(cacheCode == null || !cacheCode.toString().equals(code)) {
+            return Result.fail("驗證碼錯誤!");
+        }
+        //2.判斷用戶是否存在
+        User user = query().eq("phone", phone).one();
+        //3不存在創建新用戶並保存
+        if(user == null) {
+            user = createUserWithPhone(phone);
+        }
+        //4.保存用戶至session
+
+        session.setAttribute("user", BeanUtil.copyProperties(user, UserDTO.class));
+        return Result.ok();
+    }
+
+    private User createUserWithPhone(String phone) {
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(USER_NICK_NAME_PREFIX + RandomUtil.randomString(8));
+        save(user);
+        return user;
+
+    }
 }
---
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -0,0 +1,30 @@
+package com.hmdp.utils;
+
+import com.hmdp.dto.UserDTO;
+import com.hmdp.entity.User;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //.1獲取session
+        HttpSession session = request.getSession();
+        Object user = session.getAttribute("user");
+        //2.判斷用戶存在
+        if(user == null) {
+            response.setStatus(401);
+            return false;
+        }
+        UserHolder.saveUser((UserDTO) user);
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
